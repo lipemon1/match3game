a4cab0f003a2dbaea9216469d36fb99fc4238189
Ramon Ferreira <ramon_felipe@outlook.com>
2020-07-17 08:16:27 -0300

    First atempt failed
    This is my first attempt to create our board game. It's almost there. I am creating a boarding but it's not working properly so far

---
+++ Assets/Match3Game/Scripts/Model/BoardPiece.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Match3Game.Scripts.Model
+{
+    [System.Serializable]
+    public class BoardPiece
+    {
+        public BoardSlotOption slotOption;
+        public Sprite slotSprite;
+    }
+}
---
+++ Assets/Match3Game/Scripts/Model/BoardSlot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Match3Game.Scripts.Model
+{
+    [System.Serializable]
+    public class BoardSlot
+    {
+
+        public BoardSlotOption curValue;
+        public BoardPoint point;
+
+        public BoardSlot(BoardSlotOption curValue, BoardPoint point)
+        {
+            this.curValue = curValue;
+            this.point = point;
+        }
+    }
+    
+    public enum BoardSlotOption
+    {
+        Undefined = -1,
+        Empty,
+        Cat,
+        Dog,
+        Duck,
+        Pig,
+        Panda
+    }
+}
